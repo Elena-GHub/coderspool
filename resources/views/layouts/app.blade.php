7f1816b813b6cc66e111766a666b81b730c1c105
Elena <cabjelen@yahoo.es>
2020-09-17 08:37:32 +0200

    change to relative route in app top navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,8 +23,8 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">                
-                <a href="{{Route('bootcoder.index')}}">
-                    <img src="img/F5-orange_white_S.png" /></a>                
+                <a href="{{url('/bootcoder')}}">
+                    <img src="{{url('img/F5-orange_white_S.png')}}" /></a>                
                 <a href="{{Route('bootcoder.index')}}" class="navbar-brand">Home</a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
